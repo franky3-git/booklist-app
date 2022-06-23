6b8ae821ae112ebd825654bf1e0d89703ce4dac0
franky3-git <frankndounga@yahoo.fr>
2022-06-23 21:22:10 +0100

    fix variable names and add the class Book

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const form = document.querySelector('form');
-let infos = getInfo() || [];
+let books = getBooks() || [];
 const tbody = document.querySelector('tbody');
 const modal = document.querySelector('.modal');
 
-function renderList() {
-  tbody.innerHTML = infos
-    .map((info) => {
-      return createInfo(info.title, info.author, info.isbn, info.id);
+//single book class
+class Book {
+  constructor(title, author, isbn, id) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.id = id;
+  }
+}
+
+function renderBooks() {
+  tbody.innerHTML = books
+    .map((book) => {
+      return createUIBook(book.title, book.author, book.isbn, book.id);
     })
     .join('\n');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderList();
+  renderBooks();
 });
 
 form.addEventListener('submit', (e) => {
@@ -26,23 +36,17 @@ form.addEventListener('submit', (e) => {
   }
 
   const id = Date.now();
-  const info = {
-    title: title.value,
-    author: author.value,
-    isbn: isbn.value,
-    id: id,
-  };
-  infos.push(info);
-  setInfo();
-  renderList();
-  displayModal('success', 'some info added successfully');
+
+  books.push(new Book(title.value, author.value, isbn.value, id));
+
+  rerender('book added successfully');
 
   title.value = '';
   author.value = '';
   isbn.value = '';
 });
 
-function createInfo(title, author, isbn, id) {
+function createUIBook(title, author, isbn, id) {
   return `
    <tr>
       <td>${title}</td>
@@ -57,20 +61,19 @@ tbody.addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-delete')) {
     if (confirm('Are you sure you want to delete this info?')) {
       const id = e.target.parentElement.getAttribute('data-id');
-      infos = infos.filter((info) => info.id != id);
-      setInfo();
-      renderList();
-      displayModal('success', 'info removed successfully');
+      books = books.filter((book) => book.id != id);
+
+      rerender('book removed successfully');
     }
   }
 });
 
-function setInfo() {
-  localStorage.setItem('infos', JSON.stringify(infos));
+function setBooks() {
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-function getInfo() {
-  return JSON.parse(localStorage.getItem('infos'));
+function getBooks() {
+  return JSON.parse(localStorage.getItem('books'));
 }
 
 function displayModal(type, message) {
@@ -79,4 +82,10 @@ function displayModal(type, message) {
   setTimeout(() => {
     modal.classList.remove('open', type);
   }, 2000);
+}
+
+function rerender(message) {
+  setBooks();
+  renderBooks();
+  displayModal('success', message);
 }
